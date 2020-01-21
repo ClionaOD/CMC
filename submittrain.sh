f468c42debe49a827082b48f3d775bbd64195230
ClionaOD <odoherc1@tcd.ie>
2020-01-21 11:37:15 +0000

    remove spaces

--- submittrain.sh
+++ submittrain.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#SBATCH --cpus-per-task = 32  
+#SBATCH --cpus-per-task=32  
 
 python /home/ubuntu/CMC/train_CMC.py --batch_size 256 --num_workers 32 --data_folder /movie-associations --model_path /movie-associations/saves/model-saves --tb_path /movie-associations/saves/tb-saves
 
