791ad4f9bb00258d61e4859c7aac824712dd6344
ClionaOD <odoherc1@tcd.ie>
2020-01-21 11:46:57 +0000

    include -J

--- submittrain.sh
+++ submittrain.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 #SBATCH --cpus-per-task=32  
+#SBATCH -J train_CMC
 
 python /home/ubuntu/CMC/train_CMC.py --batch_size 256 --num_workers 32 --data_folder /movie-associations --model_path /movie-associations/saves/model-saves --tb_path /movie-associations/saves/tb-saves
 
