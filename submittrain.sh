a586807f2c361abc7871ed95dbc13b744fc7efe8
ClionaOD <odoherc1@tcd.ie>
2020-01-21 11:27:51 +0000

    include slurm script

---
+++ submittrain.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#SBATCH --job-name =  train_cmc
+#SBATCH --cpus-per-task = 32  
+
+python /home/ubuntu/CMC/train_CMC.py --batch_size 256 --num_workers 32 --data_folder /movie-associations --model_path /movie-associations/saves/model-saves --tb_path /movie-associations/saves/tb-saves
+
+aws s3 sync /movie-associations/saves s3://movie-associations/parallelcluster/saves/
