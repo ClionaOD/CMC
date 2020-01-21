eb5a3a0012f67f4e5d4878920294913559368e0c
ClionaOD <odoherc1@tcd.ie>
2020-01-21 11:36:20 +0000

    remove name

--- submittrain.sh
+++ submittrain.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #
-#SBATCH --job-name =  traincmc
 #SBATCH --cpus-per-task = 32  
 
 python /home/ubuntu/CMC/train_CMC.py --batch_size 256 --num_workers 32 --data_folder /movie-associations --model_path /movie-associations/saves/model-saves --tb_path /movie-associations/saves/tb-saves
