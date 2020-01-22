24a2ff65118b66fe5d4c32d56f40eb620e84ec9f
ClionaOD <odoherc1@tcd.ie>
2020-01-22 17:00:36 +0000

    add out paths

--- submittrain.sh
+++ submittrain.sh
@@ -2,6 +2,9 @@
 #
 #SBATCH --cpus-per-task=32  
 #SBATCH -J train_CMC
+#SBATCH --output=/movie-associations/logs/slurm-%j.out
+#SBATCH --error=/movie-associations/logs/slurm-%j.err
+
 
 python /home/ubuntu/CMC/train_CMC.py --batch_size 256 --num_workers 32 --data_folder /movie-associations --model_path /movie-associations/saves/model-saves --tb_path /movie-associations/saves/tb-saves
 
