cdf7bc2d92b32d1009ed2ced747928c476db2fc4
ClionaOD <odoherc1@tcd.ie>
2020-01-23 16:01:52 +0000

    add linear probing batch

---
+++ submitval.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+#SBATCH --cpus-per-task=32  
+#SBATCH -J train_CMC
+#SBATCH --output=/movie-associations/logs/valid-%j.out
+#SBATCH --error=/movie-associations/logs/valid-%j.err
+
+
+python /home/ubuntu/CMC/LinearProbing.py --data_folder /movie-associations --save_path /movie-associations/saves/val-saves --tb_path /movie-associations/saves/tb-saves/val --model_path /movie-associations/saves/model-saves/memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_256_view_Lab/ckpt_epoch_74.pth
