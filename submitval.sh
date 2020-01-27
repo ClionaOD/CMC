7651c4091179895bb0f8f1df5df2aab58f7fc0bb
ClionaOD <odoherc1@tcd.ie>
2020-01-27 14:30:30 +0000

    change val batch size to 20

--- submitval.sh
+++ submitval.sh
@@ -6,4 +6,9 @@
 #SBATCH --error=/movie-associations/logs/valid-%j.err
 
 
-python /home/ubuntu/CMC/LinearProbing.py --data_folder /movie-associations --resume /movie-associations/saves/val-saves/calibrated_memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_256_view_Lab_bsz_256_lr_0.1_decay_0_view_Lab/ckpt_epoch_5.pth --save_path /movie-associations/saves/val-saves --tb_path /movie-associations/saves/tb-saves/val --model_path /movie-associations/saves/model-saves/memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_256_view_Lab/ckpt_epoch_74.pth
+python /home/ubuntu/CMC/LinearProbing.py --batch_size 20 \
+ --data_folder /movie-associations \
+ --resume /movie-associations/saves/val-saves/calibrated_memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_256_view_Lab_bsz_256_lr_0.1_decay_0_view_Lab/ckpt_epoch_5.pth \
+ --save_path /movie-associations/saves/val-saves \
+ --tb_path /movie-associations/saves/tb-saves/val \
+ --model_path /movie-associations/saves/model-saves/memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_256_view_Lab/ckpt_epoch_74.pth
