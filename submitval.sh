c5b82da7ebb026db54e55aa86c3230eba14ef788
ClionaOD <odoherc1@tcd.ie>
2020-01-27 14:35:18 +0000

    correct submit val scripts

--- submitval.sh
+++ submitval.sh
@@ -8,7 +8,6 @@
 
 python /home/ubuntu/CMC/LinearProbing.py --batch_size 20 \
  --data_folder /movie-associations \
- --resume /movie-associations/saves/val-saves/calibrated_memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_256_view_Lab_bsz_256_lr_0.1_decay_0_view_Lab/ckpt_epoch_5.pth \
  --save_path /movie-associations/saves/val-saves \
  --tb_path /movie-associations/saves/tb-saves/val \
  --model_path /movie-associations/saves/model-saves/memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_256_view_Lab/ckpt_epoch_74.pth
