b8d6be33b3cbc46f8808d757ef8588d89f90f01a
ClionaOD <odoherc1@tcd.ie>
2020-02-10 12:28:04 +0000

    epochs to 1

--- submitTemporalTrain.sh
+++ submitTemporalTrain.sh
@@ -10,4 +10,5 @@ python /home/ubuntu/CMC/train_CMC.py --data_folder /movie-associations/ \
  --save_path /movie-associations/saves/model-saves/temporal \
  --tb_path /movie-associations/saves/tb-saves/temporal \
  --view temporal \
- --time_lag 60
+ --time_lag 60 \
+ --epochs 1
