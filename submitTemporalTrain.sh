af8b828b31460297723190bd4d3f8c89b8bfce87
ClionaOD <odoherc1@tcd.ie>
2020-02-11 17:12:12 +0000

    resume from chkpt 9

--- submitTemporalTrain.sh
+++ submitTemporalTrain.sh
@@ -10,4 +10,5 @@ python /home/ubuntu/CMC/train_CMC.py --data_folder /movie-associations/ \
  --model_path /movie-associations/saves/model-saves/temporal \
  --tb_path /movie-associations/saves/tb-saves/temporal \
  --view temporal \
- --time_lag 60 
+ --time_lag 60 \
+ --resume /movie-associations/saves/model-saves/temporal/memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_view_temporal/ckpt_epoch_9.pth
