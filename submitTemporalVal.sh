db716d4d951445387b1e228bc6d11d6365f2bcd1
ClionaOD <odoherc1@tcd.ie>
2020-02-17 16:23:27 +0000

    more epochs

--- submitTemporalVal.sh
+++ submitTemporalVal.sh
@@ -11,5 +11,4 @@ python /home/ubuntu/CMC/LinearProbing.py --data_folder /movie-associations/image
  --tb_path /movie-associations/saves/tb-saves/val/temporal-trained \
  --model_path /movie-associations/saves/model-saves/temporal/memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_view_temporal/ckpt_epoch_220.pth \
  --view temporal \
- --epochs 5 \
- --time_lag 60
+ --epochs 5 
