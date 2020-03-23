239f05b1477a35465d142465f67ac116b7b2202e
ClionaOD <odoherc1@tcd.ie>
2020-03-23 12:47:32 +0000

    include pth

--- workstationVal1.sh
+++ workstationVal1.sh
@@ -9,8 +9,8 @@
 
 #TODO: Add model_path after training finished.
 
-python /home/clionaodoherty/CMC/LinearProbing.py \
+python3 /home/clionaodoherty/CMC/LinearProbing.py \
     --data_folder /data/ILSVRC2012 \
     --save_path /home/clionaodoherty/movie-associations/saves/Lab/imgnet-val/fully_pretrained \
     --tb_path /home/clionaodoherty/CMC/tensorboard \
-    --model_path /home/clionaodoherty/movie-associations/saves/Lab/movie-pretrain...
+    --model_path /home/clionaodoherty/movie-associations/saves/Lab/movie-pretrain/memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_view_Lab/ckpt_epoch_200.pth
