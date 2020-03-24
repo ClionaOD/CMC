2386078432ce47ced69022cc512db852325b55f1
ClionaOD <odoherc1@tcd.ie>
2020-03-24 17:33:13 +0000

    remove visible

--- workstationTrain2.sh
+++ workstationTrain2.sh
@@ -2,9 +2,8 @@
 #
 #Train the temporal CMC using implementations in Chen et al. 2020
 
-CUDA_VISIBLE_DEVICES=0,1 python3 /home/clionaodoherty/CMC/train_temporal.py \
+python3 /home/clionaodoherty/CMC/train_temporal.py \
     --data_folder /data/movie-associations \
     --model_path /home/clionaodoherty/movie-associations/saves/temporal/chen_updates/movie-pretrain \
     --tb_path /home/clionaodoherty/CMC/tensorboard \
-    --batch_size 128 \
     --epochs 1
