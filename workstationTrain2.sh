feae31b7e8529f45526fffd5cb4ba2f6a3157d3b
ClionaOD <odoherc1@tcd.ie>
2020-03-24 17:58:52 +0000

    full epochs

--- workstationTrain2.sh
+++ workstationTrain2.sh
@@ -5,5 +5,4 @@
 CUDA_VISIBLE_DEVICES=0,1 python3 /home/clionaodoherty/CMC/train_temporal.py \
     --data_folder /data/movie-associations \
     --model_path /home/clionaodoherty/movie-associations/saves/temporal/chen_updates/movie-pretrain \
-    --tb_path /home/clionaodoherty/CMC/tensorboard \
-    --epochs 1
+    --tb_path /home/clionaodoherty/CMC/tensorboard 
