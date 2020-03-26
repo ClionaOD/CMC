b310d6ed509b0f14c878e3c46e43115b7f4ab1d4
ClionaOD <odoherc1@tcd.ie>
2020-03-26 13:59:37 +0000

    resume

--- trainResume.sh
+++ trainResume.sh
@@ -6,7 +6,7 @@ CUDA_VISIBLE_DEVICES=0,1 python3 /home/clionaodoherty/CMC/train_temporal.py \
     --data_folder /data/movie-associations \
     --model_path /home/clionaodoherty/movie-associations/saves/temporal/chen_updates/movie-pretrain \
     --tb_path /home/clionaodoherty/CMC/tensorboard \
-    --resume /home/clionaodoherty/movie-associations/saves/temporal/chen_updates/movie-pretrainmemory_nce_16384_alexnet_lr_0.18_decay_1e-05_bsz_156_sec_60_view_temporal/ckpt_epoch_280.pth \
+    --resume /home/clionaodoherty/movie-associations/saves/temporal/chen_updates/movie-pretrain/memory_nce_16384_alexnet_lr_0.18_decay_1e-05_bsz_156_sec_60_view_temporal/ckpt_epoch_280.pth \
     --batch_size 156 \
     --learning_rate 0.18 \
     --epochs 400
