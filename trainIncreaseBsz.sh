b9be7c9d670690f97664b484640fd8f0eb0f8851
ClionaOD <odoherc1@tcd.ie>
2020-03-26 14:52:43 +0000

    larger bsz

---
+++ trainIncreaseBsz.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#Train the temporal CMC using original lr scaling and higher bsz
+
+CUDA_VISIBLE_DEVICES=0,1 python3 /home/clionaodoherty/CMC/train_temporal.py \
+    --data_folder /data/movie-associations \
+    --model_path /home/clionaodoherty/movie-associations/saves/temporal/chen_updates/movie-pretrain \
+    --tb_path /home/clionaodoherty/CMC/tensorboard \
+    --batch_size 512 \
+    --learning_rate 0.6 
