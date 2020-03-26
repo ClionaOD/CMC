942a5aee6486c51409aef5cd86369d1b3d382c34
ClionaOD <odoherc1@tcd.ie>
2020-03-26 21:12:26 +0000

    change decay epochs

--- trainIncreaseBsz.sh
+++ trainIncreaseBsz.sh
@@ -7,4 +7,7 @@ CUDA_VISIBLE_DEVICES=0,1 python3 /home/clionaodoherty/CMC/train_temporal.py \
     --model_path /home/clionaodoherty/movie-associations/saves/temporal/chen_updates/movie-pretrain \
     --tb_path /home/clionaodoherty/CMC/tensorboard \
     --batch_size 512 \
-    --learning_rate 0.6 
+    --learning_rate 0.6 \
+    --epochs 140 \
+    --lr_decay_epochs 60,100,120 \
+    --resume /home/clionaodoherty/movie-associations/saves/temporal/chen_updates/movie-pretrain/memory_nce_16384_alexnet_lr_0.6_decay_1e-05_bsz_512_sec_60_view_temporal/ckpt_epoch_50.pth
