7edd85c0a447c4c7e433bd1518ae371b513947ec
ClionaOD <odoherc1@tcd.ie>
2020-03-27 12:11:51 +0000

    test

--- workstationTrain1.sh
+++ workstationTrain1.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #
 #SBATCH -J train_CMC_1
-#SBATCH --output=/home/clionaodoherty/desktop/movie-associations/slurm%j.out
-#SBATCH --error=/home/clionaodoherty/desktop/movie-associations/slurm%j.err
+#SBATCH --output=/home/clionaodoherty/movie-associations/slurm%j.out
+#SBATCH --error=/home/clionaodoherty/movie-associations/slurm%j.err
 
 #This will submit the pretraining on movie dataset with Lab objective,
 #   resuming from epoch 71 in an attempt to get better acc performance comparable
 #   to that of CMC on imagenet. In my first attempt, it had not fully converged
 #   and the lr had not decayed. Previous Top-1 was 32.38 and Top-5 54.28.
 
-CUDA_VISIBLE_DEVICES=2 python3 /home/clionaodoherty/CMC/train_CMC.py \
+CUDA_VISIBLE_DEVICES=0,1 python3 /home/clionaodoherty/CMC/train_CMC.py \
     --data_folder /data/movie-associations \
     --model_path /home/clionaodoherty/movie-associations/saves/Lab/movie-pretrain \
     --tb_path /home/clionaodoherty/CMC/tensorboard \
