6504504555c601a1e83b95ca11850be8384f0588
ClionaOD <odoherc1@tcd.ie>
2020-03-27 22:50:18 +0000

    update for slurm

--- trainCMC_5min.sh
+++ trainCMC_5min.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 #
-#Use original Lab Training but on 5 min timelag
+#Use original Temporal Training but on 5 min timelag
+#
+#SBATCH --output=/home/clionaodoherty/movie-associations/logs/slurm%j.out
+#SBATCH --error=/home/clionaodoherty/movie-associations/logs/slurm%j.err
+#SBATCH --cpus-per-task=12
+#SBATCH --gres=gpu:2
 
-CUDA_VISIBLE_DEVICES=0,1 python3 /home/clionaodoherty/CMC/train_CMC.py \
+
+python3 /home/clionaodoherty/CMC/train_CMC.py \
     --data_folder /data/movie-associations \
     --model_path /home/clionaodoherty/movie-associations/saves/temporal/movie-pretrain-5min \
     --tb_path /home/clionaodoherty/CMC/tensorboard \
     --time_lag 300 \
-    --view temporal
+    --view temporal \
+    --resume /home/clionaodoherty/movie-associations/saves/temporal/movie-pretrain-5min/memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_sec_300_view_temporal_view_temporal/ckpt_epoch_14.pth
