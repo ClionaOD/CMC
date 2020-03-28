8edfbcfe0d2f11355a8e640138ff8429402dadc9
ClionaOD <odoherc1@tcd.ie>
2020-03-28 16:49:28 +0000

    correct resume path

--- trainCMC_5min.sh
+++ trainCMC_5min.sh
@@ -13,4 +13,4 @@ python3 /home/clionaodoherty/CMC/train_CMC.py \
     --tb_path /home/clionaodoherty/CMC/tensorboard \
     --time_lag 300 \
     --view temporal \
-    --resume /home/clionaodoherty/movie-associations/saves/temporal/movie-pretrain-5min/memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_sec_300_view_temporal_view_temporal/ckpt_epoch_46.pth
+    --resume /home/clionaodoherty/movie-associations/saves/temporal/movie-pretrain-5min/memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_sec_300_view_temporal/ckpt_epoch_46.pth
