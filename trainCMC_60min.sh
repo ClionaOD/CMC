24cf5fee56763fee3530ed8239cda0113fbf2c56
ClionaOD <odoherc1@tcd.ie>
2020-03-28 16:56:55 +0000

    cuda

--- trainCMC_60min.sh
+++ trainCMC_60min.sh
@@ -7,7 +7,7 @@
 #SBATCH --gres=gpu:2
 
 
-python3 /home/clionaodoherty/CMC/train_CMC.py \
+CUDA_VISIBLE_DEVICES=2,3 python3 /home/clionaodoherty/CMC/train_CMC.py \
     --data_folder /data/movie-associations \
     --model_path /home/clionaodoherty/movie-associations/saves/temporal/movie-pretrain-60min \
     --tb_path /home/clionaodoherty/CMC/tensorboard \
