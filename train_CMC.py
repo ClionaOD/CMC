d3e63ea0cd002fbb5ff37907c76c83fa8fb56846
ClionaOD <odoherc1@tcd.ie>
2020-04-03 17:00:02 +0100

    parser

--- train_CMC.py
+++ train_CMC.py
@@ -93,7 +93,7 @@ def parse_option():
     parser.add_argument('--time_lag', type=int, default=100, help='number of 1 second frames to lag by')
 
     # use ImageNet pretrained AlexNet
-    parse.add_argument('--pretrained', type=bool, default=False, help='whether to load a pretrained model for temporal finetuning')
+    parser.add_argument('--pretrained', type=bool, default=False, help='whether to load a pretrained model for temporal finetuning')
 
     opt = parser.parse_args()
 
