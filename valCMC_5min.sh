01ac3836c272a020d1871847192f6c83a180b326
ClionaOD <odoherc1@tcd.ie>
2020-04-06 16:53:09 +0100

    Lab path

--- valCMC_5min.sh
+++ valCMC_5min.sh
@@ -11,5 +11,5 @@ python3 /home/clionaodoherty/CMC/LinearProbing.py \
     --save_path /home/clionaodoherty/movie-associations/saves/temporal/imgnet-val-5min \
     --tb_path /home/clionaodoherty/CMC/tensorboard/val \
     #--model_path /home/clionaodoherty/movie-associations/saves/temporal/movie-pretrain-5min/memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_sec_300_view_temporal/ckpt_epoch_220.pth \
-    --view Lab
+    --view Lab \
     --model_path /home/clionaodoherty/movie-associations/saves/Lab/movie-pretrain/memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_view_Lab/ckpt_epoch_200.pth
