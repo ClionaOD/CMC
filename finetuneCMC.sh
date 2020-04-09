ad53be1972abc8a2ed8ad7a648ce0a23a27d47a2
ClionaOD <odoherc1@tcd.ie>
2020-04-09 10:04:51 +0100

    chg decay and resume

--- finetuneCMC.sh
+++ finetuneCMC.sh
@@ -14,4 +14,7 @@ python3 /home/clionaodoherty/CMC/train_CMC.py \
     --tb_path /home/clionaodoherty/CMC/tensorboard/finetune \
     --time_lag 1 \
     --view temporal \
-    --pretrained /home/clionaodoherty/movie-associations/saves/Lab_pretrained_fullAlexNet.pth
+    --pretrained /home/clionaodoherty/movie-associations/saves/Lab_pretrained_fullAlexNet.pth \
+    --lr_decay_epochs 30,50,70 \
+    --epochs 100 \
+    --resume /home/clionaodoherty/movie-associations/saves/temporal/finetune5min/pretrained_memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_sec_1_view_temporal/ckpt_epoch_24.pth
