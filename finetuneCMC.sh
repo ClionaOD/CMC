24ae2029e626fd18e2320724eb886888ebd1f14f
ClionaOD <odoherc1@tcd.ie>
2020-04-14 15:30:02 +0100

    5sec

--- finetuneCMC.sh
+++ finetuneCMC.sh
@@ -2,19 +2,18 @@
 #
 #Use original Temporal Training but on Lab pretrained AlexNet
 #
-#SBATCH --output=/home/clionaodoherty/movie-associations/logs/fineTune_5min%j.out
-#SBATCH --error=/home/clionaodoherty/movie-associations/logs/fineTune_5min%j.err
+#SBATCH --output=/home/clionaodoherty/movie-associations/logs/fineTune_5sec%j.out
+#SBATCH --error=/home/clionaodoherty/movie-associations/logs/fineTune_5sec%j.err
 #SBATCH --gres=gpu:2
 #SBATCH --cpus-per-task=12
 
 
 python3 /home/clionaodoherty/CMC/train_CMC.py \
     --data_folder /data/movie-associations \
-    --model_path /home/clionaodoherty/movie-associations/saves/temporal/finetune5min \
-    --tb_path /home/clionaodoherty/CMC/tensorboard/finetune \
-    --time_lag 1 \
+    --model_path /home/clionaodoherty/movie-associations/saves/temporal/finetune5sec \
+    --tb_path /home/clionaodoherty/CMC/tensorboard/finetune/5sec \
+    --time_lag 5 \
     --view temporal \
     --pretrained /home/clionaodoherty/movie-associations/saves/Lab_pretrained_fullAlexNet.pth \
     --lr_decay_epochs 30,50,70 \
-    --epochs 100 \
-    --resume /home/clionaodoherty/movie-associations/saves/temporal/finetune5min/pretrained_memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_sec_1_view_temporal/ckpt_epoch_24.pth
+    --epochs 80 
