4d9eca07b108cadd11c80be9e6c8264f31e44d20
ClionaOD <odoherc1@tcd.ie>
2020-04-16 18:07:32 +0100

    find synsets

--- visualiseWeights.py
+++ visualiseWeights.py
@@ -1,118 +1,62 @@
-import os
-import time
-import collections
-import argparse
-
+import pickle
 import torch
 import torch.nn as nn
-from torchvision import transforms, datasets
-from util import AverageMeter
-
-from scipy.spatial import distance
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.manifold import TSNE
-
+from torchvision import models, transforms, datasets
 from models.alexnet import TemporalAlexNetCMC
-from dataset import ImageFolderInstance
 from dataset import RGB2Lab
+from nltk.corpus import wordnet as wn
 
-def parse_option():
-    parser = argparse.ArgumentParser('argument for training')
+#freq items
+with open('./freq_order.pickle','rb') as f:
+    order = pickle.load(f)
 
-    parser.add_argument('--model_path', type=str, default=None, help='path of model to visualise')
-    parser.add_argument('--data_folder', type=str, default=None, help='path to data')
-    parser.add_argument('--batch_size', type=int, default=1, help='batch_size')
-    parser.add_argument('--num_workers', type=int, default=32, help='num of workers to use')
-    parser.add_argument('--epochs', type=int, default=1, help='number of training epochs')
-    parser.add_argument('--gpu', default=None, type=int, help='GPU id to use.')
+with open('./synset_df.pickle','rb') as f:
+    synsets = pickle.load(f)
 
-    opt = parser.parse_args()
+#Load the pretrained model and set it to eval mode
+model_path = '/home/clionaodoherty/movie-associations/saves/temporal/finetune1sec/movie-training-1sec/pretrained_memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_sec_1_view_temporal/ckpt_epoch_80.pth'
+model = TemporalAlexNetCMC()
+ckpt = torch.load(model_path)
+model.load_state_dict(ckpt['model'])
+model.cuda()
+model.eval()
 
-    return opt
-
-def set_model(args):
-    model = TemporalAlexNetCMC()
-    print('==> loading pre-trained model')
-    ckpt = torch.load(args.model_path)
-    model.load_state_dict(ckpt['model'])
-    print("==> loaded checkpoint '{}' (epoch {})".format(args.model_path, ckpt['epoch']))
-    print('==> done')
-
-    model = model.cuda()
-    model.eval()
-
-    return model
-
-def get_loader(args):
-    folder = os.path.join(args.data_folder, 'val_in_folders')
+#Define the transform and dataloader
+folder = '/data/ILSVRC2012/val_in_folders/n03450230/'
     
-    mean = [(0 + 100) / 2, (-86.183 + 98.233) / 2, (-107.857 + 94.478) / 2]
-    std = [(100 - 0) / 2, (86.183 + 98.233) / 2, (107.857 + 94.478) / 2]
-    color_transfer = RGB2Lab()
+mean = [(0 + 100) / 2, (-86.183 + 98.233) / 2, (-107.857 + 94.478) / 2]
+std = [(100 - 0) / 2, (86.183 + 98.233) / 2, (107.857 + 94.478) / 2]
+color_transfer = RGB2Lab()
 
-    normalize = transforms.Normalize(mean=mean, std=std)
-    train_transform = transforms.Compose([
-        transforms.RandomResizedCrop(224, scale=(args.crop_low, 1.)),
-        transforms.RandomHorizontalFlip(),
-        color_transfer,
-        transforms.ToTensor(),
-        normalize,
-    ])
+normalize = transforms.Normalize(mean=mean, std=std)
+train_transform = transforms.Compose([
+    transforms.RandomResizedCrop(224, scale=(0.2, 1.)),
+    transforms.RandomHorizontalFlip(),
+    color_transfer,
+    transforms.ToTensor(),
+    normalize,
+])
 
-    dataset = datasets.ImageFolder(
-        folder,
-        transform=train_transform)
+#This will return (sample, class_index) as transformed by above
+dataset = datasets.ImageFolder(
+    folder,
+    transform=train_transform)
 
-    loader = torch.utils.data.DataLoader(
-        dataset, batch_size=args.batch_size, shuffle=False,
-        num_workers=args.num_workers, pin_memory=True)
+loader = torch.utils.data.DataLoader(
+    dataset, batch_size=1, shuffle=False)
 
-    return loader
+activation = {}
+def get_activation(name):
+    def hook(model, input, output):
+        activation[name] = output.detach()
+    return hook
 
-def compute_features(loader, model, opt):
-    print('Compute features')
-    model.eval()
-    act = {}
+model.encoder.module.alexnet.fc7.register_forward_hook(get_activation('fc7'))
 
-    def _store_feats(layer, inp, output):
-        """An ugly but effective way of accessing intermediate model features
-        """
-        _model_feats.append(output.cpu().numpy())
+for idx, (input, target) in enumerate(loader):
+    input = input.float()
+    input = input.cuda()
+    feat = model(input)
 
-    for m in model.features.modules():
-        if isinstance(m, nn.ReLU):
-            m.register_forward_hook(_store_feats)
-
-    for m in model.classifier.modules():
-        if isinstance(m, nn.ReLU):
-            m.register_forward_hook(_store_feats)
-
-    for i, (input, target) in enumerate(loader):
-        with torch.no_grad():
-            input = input.float()
-            if opt.gpu is not None:
-                input = input.cuda(opt.gpu, non_blocking=True)
-            target = target.cuda(opt.gpu, non_blocking=True)
-            
-            _model_feats = []
-            aux = model(input).data.cpu().numpy()
-            act[target[0]] = _model_feats
-            print(i)
-
-    return act
-
-def get_activations(loader, model, opt):
-    features = compute_features(loader, model, opt)
-    return features
-
-def main():
-    args = parse_option()
-    loader = get_loader(args)
-    model = set_model(args)
-
-if __name__ == "__main__":
-    model_path = '/home/clionaodoherty/movie-associations/saves/temporal/finetune1sec/movie-training-1sec/pretrained_memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_sec_1_view_temporal/ckpt_epoch_80.pth'
-    model = set_model()
-    loader = get_loader()
+        
 
