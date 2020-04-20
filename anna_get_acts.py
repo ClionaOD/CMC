baf2e20483cd911ac11ae8147752b4285fa0307b
ClionaOD <odoherc1@tcd.ie>
2020-04-20 11:39:07 +0100

    use for my model

--- anna_get_acts.py
+++ anna_get_acts.py
@@ -25,41 +25,6 @@ from util import AverageMeter
 from dataset import RGB2Lab
 from models.alexnet import TemporalAlexNetCMC
 
-parser = argparse.ArgumentParser(description='PyTorch Implementation of DeepCluster')
-
-parser.add_argument('data', metavar='DIR', help='path to dataset')
-parser.add_argument('--arch', '-a', type=str, metavar='ARCH',
-                    choices=['alexnet', 'vgg16'], default='alexnet',
-                    help='CNN architecture (default: alexnet)')
-parser.add_argument('--sobel', action='store_true', help='Sobel filtering')
-parser.add_argument('--clustering', type=str, choices=['Kmeans', 'PIC'],
-                    default='Kmeans', help='clustering algorithm (default: Kmeans)')
-parser.add_argument('--nmb_cluster', '--k', type=int, default=10000,
-                    help='number of cluster for k-means (default: 10000)')
-parser.add_argument('--lr', default=0.05, type=float,
-                    help='learning rate (default: 0.05)')
-parser.add_argument('--wd', default=-5, type=float,
-                    help='weight decay pow (default: -5)')
-parser.add_argument('--reassign', type=float, default=1.,
-                    help="""how many epochs of training between two consecutive
-                    reassignments of clusters (default: 1)""")
-parser.add_argument('--workers', default=4, type=int,
-                    help='number of data loading workers (default: 4)')
-parser.add_argument('--epochs', type=int, default=200,
-                    help='number of total epochs to run (default: 200)')
-parser.add_argument('--start_epoch', default=0, type=int,
-                    help='manual epoch number (useful on restarts) (default: 0)')
-parser.add_argument('--batch', default=256, type=int,
-                    help='mini-batch size (default: 256)')
-parser.add_argument('--momentum', default=0.9, type=float, help='momentum (default: 0.9)')
-parser.add_argument('--resume', default='', type=str, metavar='PATH',
-                    help='path to checkpoint (default: None)')
-parser.add_argument('--checkpoints', type=int, default=25000,
-                    help='how many iterations between two checkpoints (default: 25000)')
-parser.add_argument('--seed', type=int, default=31, help='random seed (default: 31)')
-parser.add_argument('--exp', type=str, default='', help='path to exp folder')
-parser.add_argument('--verbose', action='store_true', help='chatty')
-
 
 class ImageFolderWithPaths(datasets.ImageFolder):
     """Custom dataset that includes image file paths. Extends
@@ -77,8 +42,6 @@ class ImageFolderWithPaths(datasets.ImageFolder):
 
 def compute_features(dataloader, model, N):
     print('Compute features')
-    batch_time = AverageMeter()
-    end = time.time()
     model.eval()
     act = {}
 
@@ -87,13 +50,13 @@ def compute_features(dataloader, model, N):
         """
         _model_feats.append(output.cpu().numpy())
 
-    for m in model.features.modules():
+    for m in model.encoder.modules():
         if isinstance(m, nn.ReLU):
             m.register_forward_hook(_store_feats)
 
-    for m in model.classifier.modules():
-        if isinstance(m, nn.ReLU):
-            m.register_forward_hook(_store_feats)
+    #for m in model.classifier.modules():
+    #    if isinstance(m, nn.ReLU):
+    #        m.register_forward_hook(_store_feats)
 
     for i, input_tensor in enumerate(dataloader):
         with torch.no_grad():
@@ -118,13 +81,16 @@ def get_activations(offset):
         transforms.ToTensor(),
         normalize,
     ])
+    
     dataset = ImageFolderWithPaths(offset, transform=train_transform)
     dataloader = torch.utils.data.DataLoader(dataset,
                                             batch_size=1,
                                             num_workers=0,
                                             pin_memory=True,
                                             shuffle = False)
+    
     features = compute_features(dataloader, model, len(dataset))
+    
     return features
 
 
