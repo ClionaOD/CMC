bf2aa62ed1173eb31446527895451c2c9f2e5d49
ClionaOD <odoherc1@tcd.ie>
2020-04-21 11:02:33 +0100

    Lab split fine tune

---
+++ slurm_scripts/finetuning/fineTune_LabSplit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#Use original Temporal Training but on Lab pretrained AlexNet, split Lab across time
+#
+#SBATCH --output=/home/clionaodoherty/movie-associations/logs/fineTune_split_1sec%j.out
+#SBATCH --error=/home/clionaodoherty/movie-associations/logs/fineTune_split_1sec%j.err
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-task=12
+
+
+python3 /home/clionaodoherty/CMC/train_CMC.py \
+    --data_folder /data/movie-associations \
+    --model_path /home/clionaodoherty/movie-associations/saves/temporal/finetune1sec/Lab_Split \
+    --tb_path /home/clionaodoherty/CMC/tensorboard/finetune/1sec/lab_split \
+    --time_lag 1 \
+    --view temporal \
+    --split_Lab True \
+    --pretrained /home/clionaodoherty/movie-associations/saves/Lab_pretrained_fullAlexNet.pth \
+    --lr_decay_epochs 30,50,70 \
+    --epochs 80 
