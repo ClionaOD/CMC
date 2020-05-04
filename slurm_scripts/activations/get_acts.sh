293f84b5735ce409a695f79ebc291ca87abd58c1
ClionaOD <odoherc1@tcd.ie>
2020-05-04 12:58:22 +0100

    acts for temporal with Lab exemplars

--- slurm_scripts/activations/get_acts.sh
+++ slurm_scripts/activations/get_acts.sh
@@ -2,6 +2,6 @@
 #
 
 python3 /home/clionaodoherty/CMC/anna_get_acts.py \
-    --model_path /data/movie-associations/saves/temporal/finetune1sec/movie-training-distorted/pretrained_memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_sec_1_view_temporal/ckpt_epoch_80.pth \
-    --save_path /home/clionaodoherty/CMC/activations/temporal_lab/act_only/1sec.pickle \
+    --model_path /data/movie-associations/saves/temporal/finetune60sec/movie-training-distorted/pretrained_memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_sec_60_view_temporal/ckpt_epoch_80.pth \
+    --save_path /home/clionaodoherty/CMC/activations/temporal_lab/act_only/60sec.pickle \
     --transform Lab
