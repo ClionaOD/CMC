d0e1f46cad530a1d906dc56cbd0c8e0dc4bedda2
ClionaOD <odoherc1@tcd.ie>
2020-05-06 18:21:28 +0100

    syntax error

--- slurm_scripts/validation/valDistortPretrain_60sec.sh
+++ slurm_scripts/validation/valDistortPretrain_60sec.sh
@@ -9,6 +9,6 @@ python3 /home/clionaodoherty/CMC/LinearProbing.py \
     --data_folder /data/ILSVRC2012 \
     --save_path /data/movie-associations/saves/temporal/finetune60sec/imgnet-val \
     --tb_path /home/clionaodoherty/CMC/tensorboard/finetune/60sec_distorted/val \
-    --model_path /data/movie-associations/saves/temporal/finetune60sec/movie-training-distorted/pretrained_memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_sec_60_view_temporal/ckpt_epoch_80.pth
+    --model_path /data/movie-associations/saves/temporal/finetune60sec/movie-training-distorted/pretrained_memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_sec_60_view_temporal/ckpt_epoch_80.pth \
     --view temporal \
     --distort True 
