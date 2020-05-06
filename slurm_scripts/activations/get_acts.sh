0fceb75fda2879edd712ee96c3e1c8761ee19416
ClionaOD <odoherc1@tcd.ie>
2020-05-06 16:46:48 +0100

    fully temporal trained 1min

--- slurm_scripts/activations/get_acts.sh
+++ slurm_scripts/activations/get_acts.sh
@@ -2,6 +2,6 @@
 #
 
 python3 /home/clionaodoherty/CMC/anna_get_acts.py \
-    --model_path /data/movie-associations/saves/temporal/finetune60sec/movie-training-60sec/pretrained_memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_sec_60_view_temporal/ckpt_epoch_80.pth \
-    --save_path /home/clionaodoherty/CMC/activations/temporal_lab/60sec_lab_activations.pickle \
+    --model_path /data/movie-associations/saves/temporal/1min/movie-pretrain-1min/ckpt_epoch_220.pth \
+    --save_path /home/clionaodoherty/CMC/activations/1min_fullytrained.pickle \
     --transform Lab
