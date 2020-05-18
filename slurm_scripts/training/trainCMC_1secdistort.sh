236bf5b322bd876b5c7eecd416d3c2dbaa502060
ClionaOD <odoherc1@tcd.ie>
2020-05-18 09:34:13 +0100

    move to 10 sec

--- slurm_scripts/training/trainCMC_1secdistort.sh
+++ slurm_scripts/training/trainCMC_1secdistort.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 #
-#Use original full Temporal Training but on 1 sec timelag, distorted
+#Use original full Temporal Training but on 10 sec timelag, distorted
 #
-#SBATCH --output=/home/clionaodoherty/movie-associations/logs/1sec_fulldistort%j.out
-#SBATCH --error=/home/clionaodoherty/movie-associations/logs/1sec_fulldistort%j.err
+#SBATCH --output=/home/clionaodoherty/movie-associations/logs/10sec_fulldistort%j.out
+#SBATCH --error=/home/clionaodoherty/movie-associations/logs/10sec_fulldistort%j.err
 #SBATCH --gres=gpu:4
 #SBATCH --cpus-per-task=12
 
 
 python3 /home/clionaodoherty/CMC/train_CMC.py \
     --data_folder /data/movie-associations \
-    --model_path /data/movie-associations/saves/temporal/1sec/distorted \
-    --tb_path /home/clionaodoherty/CMC/tensorboard/1sec \
-    --time_lag 1 \
+    --model_path /data/movie-associations/saves/temporal/10sec/distorted \
+    --tb_path /home/clionaodoherty/CMC/tensorboard/10sec \
+    --time_lag 10 \
     --view temporal \
     --lr_decay_epochs 120,160,200 \
     --epochs 220 \
-    --distort True \
-    --resume /data/movie-associations/saves/temporal/1sec/distorted/memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_sec_1_view_temporal/ckpt_epoch_114.pth
+    --distort True 
