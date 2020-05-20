9cf2b5d7f39896ed4cfb4a7d5ca45901ac4a570d
ClionaOD <odoherc1@tcd.ie>
2020-05-20 09:41:21 +0100

    mv for 10 sec acts

--- slurm_scripts/activations/get_acts.sh
+++ slurm_scripts/activations/get_acts.sh
@@ -2,6 +2,6 @@
 #
 
 python3 /home/clionaodoherty/CMC/anna_get_acts.py \
-    --model_path /data/movie-associations/saves/temporal/1sec/distorted/memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_sec_1_view_temporal/ckpt_epoch_200.pth \
-    --save_path /home/clionaodoherty/CMC/activations/1sec_full_distort.pickle \
+    --model_path /data/movie-associations/saves/temporal/10sec/distorted/memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_sec_10_view_temporal/ckpt_epoch_200.pth \
+    --save_path /home/clionaodoherty/CMC/activations/full_temporal_distort/10sec_full_distort.pickle \
     --transform distort
--- slurm_scripts/training/trainCMC_10secdistort.sh
+++ slurm_scripts/training/trainCMC_10secdistort.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #
-#Use original full Temporal Training but on 10 sec timelag, distorted
+#Use original full Temporal Training but on 30 sec timelag, distorted
 #
-#SBATCH --output=/home/clionaodoherty/movie-associations/logs/10sec_fulldistort%j.out
-#SBATCH --error=/home/clionaodoherty/movie-associations/logs/10sec_fulldistort%j.err
+#SBATCH --output=/home/clionaodoherty/movie-associations/logs/30sec_fulldistort%j.out
+#SBATCH --error=/home/clionaodoherty/movie-associations/logs/30sec_fulldistort%j.err
 #SBATCH --gres=gpu:4
 #SBATCH --cpus-per-task=12
 
 
 python3 /home/clionaodoherty/CMC/train_CMC.py \
     --data_folder /data/movie-associations \
-    --model_path /data/movie-associations/saves/temporal/10sec/distorted \
-    --tb_path /home/clionaodoherty/CMC/tensorboard/10sec \
-    --time_lag 10 \
+    --model_path /data/movie-associations/saves/temporal/30sec/distorted \
+    --tb_path /home/clionaodoherty/CMC/tensorboard/30sec \
+    --time_lag 30 \
     --view temporal \
     --lr_decay_epochs 120,160,200 \
     --epochs 220 \
