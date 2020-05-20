dd6fa38136d2eeaeae6fd438f5361599bf767a72
ClionaOD <odoherc1@tcd.ie>
2020-05-20 12:11:44 +0100

    60sec

--- slurm_scripts/activations/get_acts.sh
+++ slurm_scripts/activations/get_acts.sh
@@ -2,6 +2,6 @@
 #
 
 python3 /home/clionaodoherty/CMC/anna_get_acts.py \
-    --model_path /data/movie-associations/saves/temporal/10sec/distorted/memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_sec_10_view_temporal/ckpt_epoch_200.pth \
-    --save_path /home/clionaodoherty/CMC/activations/full_temporal_distort/10sec_full_distort.pickle \
+    --model_path /data/movie-associations/saves/temporal/1min/distorted/memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_sec_60_view_temporal/ckpt_epoch_200.pth \
+    --save_path /home/clionaodoherty/CMC/activations/full_temporal_distort/60sec_full_distort.pickle \
     --transform distort
