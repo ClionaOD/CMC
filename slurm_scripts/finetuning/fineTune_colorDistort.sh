95e0a20468544ac17cd18dc7e9df5947351cbdb0
ClionaOD <odoherc1@tcd.ie>
2020-05-24 22:08:29 +0100

    5min finetune

--- slurm_scripts/finetuning/fineTune_colorDistort.sh
+++ slurm_scripts/finetuning/fineTune_colorDistort.sh
@@ -2,19 +2,18 @@
 #
 #Use original Temporal Training but on Lab pretrained AlexNet, use color distortion
 #
-#SBATCH --output=/home/clionaodoherty/movie-associations/logs/fineTune_distort_30sec%j.out
-#SBATCH --error=/home/clionaodoherty/movie-associations/logs/fineTune_distort_30sec%j.err
+#SBATCH --output=/home/clionaodoherty/movie-associations/logs/fineTune_distort_5min%j.out
+#SBATCH --error=/home/clionaodoherty/movie-associations/logs/fineTune_distort_5min%j.err
 #SBATCH --gres=gpu:2
 #SBATCH --cpus-per-task=12
 
 
 python3 /home/clionaodoherty/CMC/train_CMC.py \
     --data_folder /data/movie-associations \
-    --model_path /data/movie-associations/saves/temporal/finetune30sec/movie-training-distorted \
-    --tb_path /home/clionaodoherty/CMC/tensorboard/finetune/30sec_distorted \
-    --time_lag 30 \
+    --model_path /data/movie-associations/saves/temporal/finetune5min/movie-training-distorted \
+    --tb_path /home/clionaodoherty/CMC/tensorboard/finetune/5min_distorted \
+    --time_lag 300 \
     --view temporal \
     --lr_decay_epochs 30,50,70 \
     --epochs 80 \
-    --distort True \
-    --resume /data/movie-associations/saves/temporal/finetune30sec/movie-training-distorted/pretrained_memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_sec_30_view_temporal/ckpt_epoch_5.pth
+    --distort True 
