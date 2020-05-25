314946b879dd701c70c0a0e824dfd57ed4b13a12
ClionaOD <odoherc1@tcd.ie>
2020-05-25 19:23:50 +0100

    5min acts

--- slurm_scripts/activations/get_acts.sh
+++ slurm_scripts/activations/get_acts.sh
@@ -2,6 +2,6 @@
 #
 
 python3 /home/clionaodoherty/CMC/anna_get_acts.py \
-    --model_path /data/movie-associations/saves/movie_Lab.pth \
-    --save_path /home/clionaodoherty/CMC/activations/movie_lab.pickle \
-    --transform Lab
+    --model_path /data/movie-associations/saves/temporal/finetune5min/movie-training-distorted/memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_sec_300_view_temporal/ckpt_epoch_80.pth \
+    --save_path /home/clionaodoherty/CMC/activations/main/5mindistort_acts.pickle \
+    --transform distort
