217751192dd6b347d510f4ad0a87d3104714aef6
ClionaOD <odoherc1@tcd.ie>
2020-05-25 12:07:58 +0100

    movie Lab acts

--- slurm_scripts/activations/get_acts.sh
+++ slurm_scripts/activations/get_acts.sh
@@ -2,6 +2,6 @@
 #
 
 python3 /home/clionaodoherty/CMC/anna_get_acts.py \
-    --model_path /data/movie-associations/saves/temporal/30sec/distorted/memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_sec_30_view_temporal/ckpt_epoch_220.pth \
-    --save_path /home/clionaodoherty/CMC/activations/full_temporal_distort/30sec_full_distort.pickle \
-    --transform distort
+    --model_path /data/movie-associations/saves/Lab/movie-pretrain/memory_nce_16384_alexnet_lr_0.03_decay_0.0001_bsz_128_view_Lab/ckpt_epoch_200.pth \
+    --save_path /home/clionaodoherty/CMC/activations/movie_lab.pickle \
+    --transform Lab
